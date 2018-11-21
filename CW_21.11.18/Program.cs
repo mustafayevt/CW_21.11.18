b5ee1d43a765eaf5f5d9862cb98cfccd8a267dad
Tural Mustafayev <turaltural99@gmail.com>
2018-11-21 19:46:42 +0400

    task1

--- CW_21.11.18/Program.cs
+++ CW_21.11.18/Program.cs
@@ -10,6 +10,35 @@ namespace CW_21._11._18
     {
         static void Main(string[] args)
         {
+            MyList  <int>list = new MyList<int>();
+            list.Add(5);
+            list.Add(6);
+            list.Show();
+            Console.WriteLine(list.Contains(5));
+        }
+    }
+    class MyList<T>
+    {
+        public T []GetT=new T[0];
+        public void Add(T tmp)
+        {
+            Array.Resize(ref GetT, GetT.Length + 1);
+            GetT[GetT.Length - 1] = tmp;
+        }
+        public bool Contains(T tmp)
+        {
+            for (int i = 0; i < GetT.Length; i++)
+            {
+                if (GetT[i].Equals(tmp)) return true;
+            }
+            return false;
+        }
+        public  void Show()
+        {
+            for (int i = 0; i < GetT.Length; i++)
+            {
+                Console.WriteLine(GetT[i]);
+            }
         }
     }
 }
